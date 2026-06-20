c7a8bb900ba786efe220fd1ca7ecb230c0aa620a
subinsudkuo-source <subinsudkuo@gmail.com>
2026-06-20 11:43:13 +0700

    Point Vercel config to new Apps Script deployment

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwsWeRZDYzGV3Y0Dh-FODAQgBuk0s5yiJL-8mturr4NXbjOZxPpKJsvgREKzWm_crqq/exec";
+const DEFAULT_APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwoBZYkXTRDKhjBA5a6c5JIK34d4tWCc5mmMPG9ItxGms3yideRMqP_YSmlnyC7wjJs/exec";
 
 export default function handler(request, response) {
   response.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=300");
